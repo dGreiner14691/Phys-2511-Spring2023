f1ff43977054a78d3de2399b8c0618f14985b3c9
dGreiner14691 <123116005+dGreiner14691@users.noreply.github.com>
2023-01-26 23:33:39 -0500

    Add files via upload

---
+++ Lab1-Magic8Ball/lab1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+from random import *
+from time import *
+
+print("please ask a question:")
+reply = input()
+sleep(5)
+print("thinking...")
+sleep(5)
+print("your question was:", reply)
+sleep(5)
+print("my answer is:", choice(['yes!', 'no!']) )
+
+
+# In[ ]:
+
+
+
+
